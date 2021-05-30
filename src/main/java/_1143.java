5c58bc99a8e3ad9f045422292ba9a5e78f1005ca
vineetap <pawar.vineeta@gmail.com>
2021-05-30 10:43:45 +0530

    More questions

--- src/main/java/_1143.java
+++ src/main/java/_1143.java
@@ -1,6 +1,7 @@
 package main.java;
 
 public class _1143 {
+    // With two 1D arrays
         public int longestCommonSubsequence(String text1, String text2) {
             if(text2.length()<text1.length()){
                 String temp=text1;
@@ -31,5 +32,27 @@ public class _1143 {
             }
 
             return previous[len1];
+        }
+
+        // With 2D array
+        public int longestCommonSubsequence2(String text1, String text2) {
+            int len1=text1.length();
+            int len2=text2.length();
+
+            int[][] dp=new int[len1+1][len2+1];
+
+            for(int r=0;r<=len1;r++){
+                for(int c=0;c<=len2;c++){
+
+                    if(r==0||c==0)
+                        dp[r][c]=0;
+                    else
+                        dp[r][c]=text1.charAt(r-1)==text2.charAt(c-1)?1+dp[r-1][c-1]:
+                                Math.max(dp[r-1][c],dp[r][c-1]);
+                }
+            }
+
+            return dp[len1][len2];
+
         }
     }
